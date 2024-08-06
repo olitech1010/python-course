7ebb1a10285d4d32d462d786ac781301889a7afe
Clement Mensah <olitech1010@gmail.com>
2024-08-06 15:50:27 +0000

    variables

---
+++ mosh_course/haggith.py
@@ -0,0 +1,26 @@
+# print("haggith is pretty")
+# print("clement is not being nice to me today")
+# print("how can i complain to god about my heart")
+# print("life will get better")
+# print("never will i cry")
+
+
+# print("if you come to school late i will punish you tomorrow i will go to the cinema")
+
+# x = 3
+# y = 7
+# if x == 3:
+#     print("x is 3")
+#     print(x)
+# elif x == 4 :
+#     print("x is 4")
+
+
+prices = [10, 5, 3, 8, 11]
+highest = prices[0]
+for price in prices:
+    if price>highest:
+        highest = price
+print(highest)
+
+
