b9d2b2feb4600cfac539408067e1643f76f8897f
Clement Mensah <olitech1010@gmail.com>
2024-08-07 01:24:37 +0000

    Update haggith.py

--- mosh_course/haggith.py
+++ mosh_course/haggith.py
@@ -16,11 +16,57 @@
 #     print("x is 4")
 
 
-prices = [10, 5, 3, 8, 11]
-highest = prices[0]
-for price in prices:
-    if price>highest:
-        highest = price
-print(highest)
+# prices = [10, 5, 3, 8, 11]
+# highest = prices[0]
+# for price in prices:
+#     if price>highest:
+#         highest = price
+# print(highest)
+
+
+
+# matrix = [
+#     [0, 1, 2, 3],
+#     [4, 5, 6, 7],
+#     [8, 9, 10],
+# ]
+# check =0
+# for row in matrix:
+#     for item in row:
+#         print(item)
+# print()
+
+
+my_list = [1,4,67,45,7,4,7,8,5,42,1,7,9]
+#.append, .pop, .sort .reverse .copy .index .insert .remove .clear .count
+
+# my_list.append(20)
+# my_list.insert(0,20)
+# my_list.remove(1)
+# print(my_list)
+
+# #remove duplicates in a list
+
+# unique=[]
+# for number in my_list:
+#     if number not in unique:
+#         unique.append(number)
+# print(unique)
+# unique.sort()
+# unique.reverse()
+# print(unique)
+
+# #tuples
+# coordianates = (3, 5, 7)
+# x = coordianates[0]
+# y = coordianates[1]
+# z = coordianates[2]
+
+# #unpacking
+# x, y, z = coordianates
+# print(coordianates)
+# for values in coordianates:
+#     print(values)
+# print(x*y*z)
 
 
