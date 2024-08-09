62f0c81e7df7b3a73cebf38fffa087bb3b0f7c2f
Clement Mensah <olitech1010@gmail.com>
2024-08-09 02:06:17 +0000

    random package

--- mosh_course/app.py
+++ mosh_course/app.py
@@ -1,7 +1,23 @@
-from utils import find_max
-from ecommerce import shipping
+# from utils import find_max
+# from ecommerce import shipping
 
-first_student = find_max([45,45,64,64,46,7,45,7,49,78])
-print (first_student)
+# first_student = find_max([45,45,64,64,46,7,45,7,49,78])
+# print (first_student)
 
-shipping.calc_shipping()
+# shipping.calc_shipping()
+
+import random
+
+for i in range(30):
+    random_number = int(random.random()*100)
+    print(random_number)
+members = ["Clement", "Olives", "John", "Stephen", "Haggith"]
+
+leader = random.choice(members)
+print(leader)
+
+from utils import Dice
+
+dice = Dice
+roll_dice = dice.roll()
+print(roll_dice)
--- mosh_course/utils.py
+++ mosh_course/utils.py
@@ -1,6 +1,13 @@
+import random
 def find_max(numbers):
     max = numbers[0]
     for number in numbers:
         if number > max:
             max=number
     return max
+
+class Dice:
+    def roll():
+        first_number = random.randint(1,6)
+        second_number = random.randint(1,6)
+        return first_number, second_number
