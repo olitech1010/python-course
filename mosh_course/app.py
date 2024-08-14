90c11103abe96086f7f976c3d52679975f77372f
Clement Mensah <olitech1010@gmail.com>
2024-08-14 01:56:26 +0000

    Path

--- mosh_course/app.py
+++ mosh_course/app.py
@@ -6,18 +6,30 @@
 
 # shipping.calc_shipping()
 
-import random
+# import random
 
-for i in range(30):
-    random_number = int(random.random()*100)
-    print(random_number)
-members = ["Clement", "Olives", "John", "Stephen", "Haggith"]
+# for i in range(30):
+#     random_number = int(random.random()*100)
+#     print(random_number)
+# members = ["Clement", "Olives", "John", "Stephen", "Haggith"]
 
-leader = random.choice(members)
-print(leader)
+# leader = random.choice(members)
+# print(leader)
 
-from utils import Dice
+# from utils import Dice
 
-dice = Dice
-roll_dice = dice.roll()
-print(roll_dice)
+# dice = Dice
+# roll_dice = dice.roll()
+# print(roll_dice)
+
+#PATH
+
+from pathlib import Path
+
+path = Path("emails")
+if not path.exists():
+    path.mkdir()
+print(path.exists())
+path_directory = Path()
+for file in path_directory.glob("*"):
+    print(file)
--- mosh_course/utils.py
+++ mosh_course/utils.py
@@ -1,10 +1,13 @@
 import random
+
+
 def find_max(numbers):
     max = numbers[0]
     for number in numbers:
         if number > max:
             max=number
     return max
+
 
 class Dice:
     def roll():
